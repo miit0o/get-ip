17e06cfaac312d35e578d8877504795761f7a847
miit0o <christoph@rustige.me>
2020-10-22 16:32:19 +0200

    added functionality

--- src/get-ip/get-ip.php
+++ src/get-ip/get-ip.php
@@ -13,4 +13,18 @@ License: MIT
 
 Copyright (c) 2020 Christoph Rustige. All rights reserved.
 */
+
+function get_ip() {
+	if ( ! empty( $_SERVER['HTTP_CLIENT_IP'] ) ) {
+	    $ip = $_SERVER['HTTP_CLIENT_IP'];
+	} elseif ( ! empty( $_SERVER['HTTP_X_FORWARDED_FOR'] ) ) {
+	    $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+	} else {
+	    $ip = $_SERVER['REMOTE_ADDR'];
+	}
+	    return apply_filters( 'wpb_get_ip', $ip );
+	}
+
+	add_shortcode('show_ip', 'get_ip');
+
 ?>
