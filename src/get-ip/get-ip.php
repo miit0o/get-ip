f4561c72c613ba88e1b2a6e244b5baecdd6fd9b0
miit0o <christoph@rustige.me>
2020-10-22 16:09:37 +0200

    added plugin information

---
+++ src/get-ip/get-ip.php
@@ -0,0 +1,16 @@
+<?php
+/*
+Plugin Name: Get-IP
+Plugin URI: https://github.com/miit0o/get-ip
+Description: A simple plugin to enable showing the current users IP address. You can use the shortcode [show_ip] to view the users IP address on any page.
+Version: 0.1
+Requires at least: 4.8
+Tested up to: 5.5
+Requires PHP: 5.6
+Author: miit0o
+Author URI: https://rustige.me
+License: MIT
+
+Copyright (c) 2020 Christoph Rustige. All rights reserved.
+*/
+?>
